fb9250a7af1ac2348ed3ea459632e01103cf79d4
GraceBruketa <Grace.Bruketa@edu.sait.ca>
2024-09-20 17:19:41 -0600

    Initial commit

---
+++ app/page.js
@@ -0,0 +1,13 @@
+import Link from 'next/link';
+export default function Page() {
+    return (
+      <main>
+        <h1 className= "text-4x1 font-bold mb-2">CPRG 306: Web Development 2 - Assignments</h1>
+        <p>
+          <Link href="http://localhost:3000/week-2" target="_blank">
+            week-2
+          </Link>
+        </p>
+      </main>
+    );
+}
---
+++ app/week-2/student-info.js
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+const StudentInfo = () => {
+    return (
+      <div>
+        <h2>Grace Bruketa</h2>
+        <p>
+          <Link href="https://github.com/GraceBruketa" target="_blank">
+            My GitHub 
+          </Link>
+        </p>
+      </div>
+    );
+  };
+  
+  export default StudentInfo;
