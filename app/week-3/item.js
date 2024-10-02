9392bc71c084cf79026103f431ce819c5115999e
GraceBruketa <Grace.Bruketa@edu.sait.ca>
2024-10-01 18:12:29 -0600

    slight CSS change

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,7 +1,7 @@
 import React from 'react';
 export default function Item (props) {
     return (    
-        <ul className= "flex flex-col justify-items-start p-2 bg-blue-950 shadow-md my-4">
+        <ul className= "flex flex-col justify-center p-2 bg-blue-950 shadow-md my-4 mx-4">
             <div>
                 <li className= "text-lg font-semibold text--300">{props.name} </li>
             </div>
